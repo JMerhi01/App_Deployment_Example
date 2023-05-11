694eb5fa9ab95854c1e91fc765f6bbe424dd7a6e
JMerhi01 <132056984+JMerhi01@users.noreply.github.com>
2023-05-11 17:28:24 +0100

    First Commit

---
+++ Vagrantfile
@@ -0,0 +1,13 @@
+
+Vagrant.configure("2") do |config|
+
+  # configures the VM settings
+  config.vm.box = "ubuntu/xenial64"
+  config.vm.network "private_network", ip:"192.168.10.100"
+
+  # provision the VM to have Nginx
+  config.vm.provision "shell", path: "provision.sh"
+
+  # put the app folder from our local machine to the VM
+  config.vm.synced_folder "app", "/home/vagrant/app"
+end
---
+++ provision.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Update packages
+sudo apt-get update -y
+sudo apt-get upgrade -y
+# Install Nginx
+sudo apt-get install nginx -y
+sudo systemctl start nginx 
+# Install Ruby and Bundler
+sudo apt-get install ruby-full -y
+sudo gem install bundler
+# Install Python properties 
+sudo apt-get install python-software-properties -y
+# Install Node.js v14.x
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt-get install -y nodejs
+# Install pm2 globally
+sudo npm install -g pm2
+# Navigate to the directory containing the app
+cd /home/vagrant/app 
+# Install dependencies from the gemfile
+bundle
+# Install dependencies from package.json
+npm install
+# Start the app
+node app.js
